c1ff2b5af72e57dfa33af61097c72c5141358d98
vibhavpan <104501131+vibhavpan@users.noreply.github.com>
2022-08-28 09:37:34 +0530

    Create TreeSet.java
    Creates and sorts a Point based TreeSet.

---
+++ TreeSet.java
@@ -0,0 +1,11 @@
+//if second point's x's value is greater then return -1
+           if(this.x<p.x)
+               return -1;
+           
+           // if second point's x's value is lesser then return 1
+           if(this.x>p.x)
+               return 1;
+           
+           //if second point's x's value equal then check for y
+           else
+           
